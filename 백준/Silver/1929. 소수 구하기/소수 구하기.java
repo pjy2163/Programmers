862f4144c86d047d9adfb8a4e2b458db75f79ff1
Jiyoung Park <69078515+pjy2163@users.noreply.github.com>
2025-08-04 19:36:42 +0900

    [Silver III] Title: 소수 구하기, Time: 1064 ms, Memory: 32972 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        for (int i = a; i <= b; i++) {
+            
+            if(i == 1) {
+                continue;
+            }
+            
+            boolean isPrime = true;
+            
+            for(int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0 ) {
+                    isPrime = false;
+                    break;
+                }
+               
+            }
+            if (isPrime) {  
+                System.out.println(i);
+            }
+        }
+    }
+} 
