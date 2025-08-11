e8b156f66e5f0ee025417c0368413973a08305e7
Jiyoung Park <69078515+pjy2163@users.noreply.github.com>
2025-08-11 10:34:37 +0900

    [Gold II] Title: 저울, Time: 112 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int target = 1;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < n; i++) {
+            if(target >= arr[i]) {
+                target += arr[i];
+            }
+            else { 
+                break;
+            }
+        }
+        
+        System.out.print(target);
+        
+    }
+}
