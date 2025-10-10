2e070bab69ec0e275082b2981d58cb57d476df77
Jiyoung Park <69078515+pjy2163@users.noreply.github.com>
2025-10-11 00:49:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.38 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,25 +1,29 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
         
-        Map<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         
-        for ( String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
+        for (String s : participant) {
+	        map.put(s, map.getOrDefault(s, 0) + 1);
+	      }
+	      
+	      for (String s : completion) {
+		      if (map.containsKey(s)) {
+			      map.put(s, map.get(s) - 1);
+			    }
         }
         
-        for (String name : completion) {
-            map.put(name, map.get(name) - 1);
+        for (String key : map.keySet()) {
+		      if (map.get(key) != 0) {
+			      answer = key;
+			      break;
+			    }
         }
         
-        for (String name : map.keySet()) {
-            if(map.get(name) != 0) {
-                return name;
-            }
-        }
         
-        return "";
+        return answer;
     }
 }
